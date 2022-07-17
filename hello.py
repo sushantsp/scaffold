0a86786996290fea88805a390b7213088a61e484
Sushant-gcp <sushantlearns@gmail.com>
2022-07-17 11:41:37 +0000

    adding gcp version

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 def add(x, y):
     return x + y
 
-# var = 
+
+def prod(x, y):
+    return x * y
+
+
+# var =
 results = add(1, 1)
 print(f"the sum is : {1} {1}, {results}")
+
+prod_results = prod(2, 2)
+print(f"the product is : {2} {2}, {prod_results}")
