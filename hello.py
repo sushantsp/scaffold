bced76a6b2d86df6258df0c6b1fbafa2c50bc028
EC2 Default User <ec2-user@ip-172-31-56-117.ec2.internal>
2022-07-17 08:16:54 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,6 @@
+def add(x, y):
+    return x + y
+
+
+results = add(1, 1)
+print(f"the sum is : {1} {1}, {results}")
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+from hello import add
+
+def test_add():
+    assert add(1,2) == 3
+    
+    
+    
